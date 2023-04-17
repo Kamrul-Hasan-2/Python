af04f2534c9bc1dfc118bad73a3c4d919998f235
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2023-04-18 03:02:22 +0600

    Add files via upload

---
+++ fn.py
@@ -0,0 +1,9 @@
+def add_three(input_var):
+    output_var = input_var + 3
+    return output_var
+
+# Run the function with 10 as input
+new_number = add_three(20)
+
+# Check that the value is 13, as expected
+print(new_number)
